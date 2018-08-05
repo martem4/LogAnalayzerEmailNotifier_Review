f2639ebf7225801e2b3870bd21cc0a3fbc980b12
nkargin <hei@spark-mail.ru>
2018-08-05 20:29:56 +0300

    Added logger support. Some refactorings.

--- src/main/java/com/century/logssender/properties/FileProperties.java
+++ src/main/java/com/century/logssender/properties/FileProperties.java
@@ -1,5 +1,8 @@
 package com.century.logssender.properties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,6 +11,7 @@ import java.util.Properties;
 public abstract class FileProperties extends Properties {
 
     private static final String LOADING_ERROR_MESSAGE = "Problem with properties loading. Class: %s";
+    private static final Logger logger = LoggerFactory.getLogger(FileProperties.class);
 
     FileProperties() {
         this.readConnectionProperties();
@@ -21,7 +25,7 @@ public abstract class FileProperties extends Properties {
             inputStream = new FileInputStream(String.format("properties/%s.properties", getFileName()));
             load(inputStream);
         } catch (IOException e) {
-            System.err.println(String.format(LOADING_ERROR_MESSAGE, this.getClass().getCanonicalName()));
+            logger.error(String.format(LOADING_ERROR_MESSAGE, this.getClass().getCanonicalName()), e);
         }
     }
 
