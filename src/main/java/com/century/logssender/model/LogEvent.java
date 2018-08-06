6375756d58a4c022f9f0d869536f665b6d0060ba
nkargin <hei@spark-mail.ru>
2018-08-06 16:37:54 +0300

    Refactored

--- src/main/java/com/century/logssender/model/LogEvent.java
+++ src/main/java/com/century/logssender/model/LogEvent.java
@@ -1,10 +1,12 @@
 package com.century.logssender.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.sql.Date;
 
 @Data
+@AllArgsConstructor
 public class LogEvent {
 
     private int id;
@@ -16,15 +18,4 @@ public class LogEvent {
     private String message;
     private String sysLogTag;
 
-    public LogEvent(int id, Date receivedAt, Date deviceReportedTime, int facility, int priority, String fromHost,
-                    String message, String sysLogTag) {
-        this.id = id;
-        this.receivedAt = receivedAt;
-        this.deviceReportedTime = deviceReportedTime;
-        this.facility = facility;
-        this.priority = priority;
-        this.fromHost = fromHost;
-        this.message = message;
-        this.sysLogTag = sysLogTag;
-    }
 }
--- src/main/java/com/century/logssender/model/Template.java
+++ src/main/java/com/century/logssender/model/Template.java
@@ -2,6 +2,7 @@ package com.century.logssender.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.util.Objects;
 import java.util.Set;
